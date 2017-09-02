2dc9238291c0a1f3b001dfac2a086144382e9f61
Torstein Bjørnstad <tosh@tosh.no>
2017-09-02 13:22:13 +0200

    Update transactionlist

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -11,14 +11,20 @@ class TransactionList extends React.Component {
       const card = this.props.cards.find(c => c.cardId === transaction.cardId);
       const machine = transaction.machine && this.props.machines.find(m => m._id === transaction.machine.id);
 
-      const CardTag = card ? <button className="button is-outlined is-small" disabled>{card.cardId}</button> : transaction.cardId;
-      const PlayerTag = player ? <button className="button is-outlined is-small" disabled>{player.name}</button> : transaction.playerId;
-      const MachineTag = machine ? <button className="button is-outlined is-small" disabled>{machine.name}</button> : transaction.machineId;
-      const Amount = <span className={transaction.amount > 0 ? 'has-text-success' : 'has-text-danger'}>{transaction.amount.toFixed(2)}</span>
+      const CardTag = card ?
+        <button className="button is-outlined is-small" disabled>{card.cardId}</button> : transaction.cardId;
+      const PlayerTag = player ?
+        <button className="button is-outlined is-small" disabled>{player.name}</button> : transaction.playerId;
+      const MachineTag = machine ?
+        <button className="button is-outlined is-small" disabled>{machine.name}</button> : transaction.machineId;
+      const Amount = transaction.amount && <span
+        className={transaction.amount > 0 ? 'has-text-success' : 'has-text-danger'}>{transaction.amount.toFixed(2)}</span>
 
       return (
         <tr>
-          <td><button className="button is-outlined is-small" disabled>{transaction.type}</button></td>
+          <td>
+            <button className="button is-outlined is-small" disabled>{transaction.type}</button>
+          </td>
           <td>{moment(transaction.timestamp).format('LLL')}</td>
           <td>{CardTag}</td>
           <td>{PlayerTag}</td>
